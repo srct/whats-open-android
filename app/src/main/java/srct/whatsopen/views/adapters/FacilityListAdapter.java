b7c65322d9eab68461b583fb259c6edc15463c4f
Robert Hitt <rhitt@gmu.edu>
2017-01-11 19:33:35 -0500

    Open facilities now display open duration

--- app/src/main/java/srct/whatsopen/views/adapters/FacilityListAdapter.java
+++ app/src/main/java/srct/whatsopen/views/adapters/FacilityListAdapter.java
@@ -3,13 +3,17 @@ package srct.whatsopen.views.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -55,9 +59,18 @@ public class FacilityListAdapter extends
             // set the RV cell to be highlighted
             holder.itemView.setBackgroundColor(ContextCompat
                     .getColor(context, R.color.facilityOpen));
+
+            // show the duration that the facility will be open
+            setItemPaddingInDp(holder.textLayout, 8);
+            holder.durationTextView.setVisibility(View.VISIBLE);
+            holder.nameTextView.setTypeface(null, Typeface.BOLD);
         } else {
             holder.itemView.setBackgroundColor(ContextCompat
                     .getColor(context, R.color.facilityClosed));
+
+            setItemPaddingInDp(holder.textLayout, 15);
+            holder.durationTextView.setVisibility(View.GONE);
+            holder.nameTextView.setTypeface(null, Typeface.NORMAL);
         }
 
         if(facility.isFavorited()) {
@@ -72,16 +85,27 @@ public class FacilityListAdapter extends
         textView.setText(facility.getName());
     }
 
+    // Helper method to set the facility item layout's padding
+    // Have to convert from pixels to dp
+    private void setItemPaddingInDp(LinearLayout layout, int paddingPx) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        int paddingTop = (int) (paddingPx*scale + 0.5f);
+        int paddingBottom = (int) (paddingPx*scale + 0.5f);
+
+        layout.setPadding(0, paddingTop, 0, paddingBottom);
+    }
+
     // Set up for the Recycler View cells
     public class ViewHolder extends RecyclerView.ViewHolder implements FacilityView {
 
-        @BindView(R.id.facility_name)
-        TextView nameTextView;
-        @BindView(R.id.favorite_button)
-        ImageButton favoriteButton;
+        @BindView(R.id.facility_name) TextView nameTextView;
+        @BindView(R.id.favorite_button) ImageButton favoriteButton;
+        @BindView(R.id.facility_duration) TextView durationTextView;
+        @BindView(R.id.text_layout) LinearLayout textLayout;
 
-        FacilityPresenter mPresenter;
-        Facility data;
+        private FacilityPresenter mPresenter;
+        private Facility data;
+        private String duration;
 
         public ViewHolder(final View itemView) {
             super(itemView);
@@ -94,6 +118,9 @@ public class FacilityListAdapter extends
             // Set up presenter
             mPresenter = new FacilityPresenter();
             mPresenter.attachView(this);
+
+            duration = mPresenter.getStatusDuration(data, Calendar.getInstance());
+            durationTextView.setText(duration);
         }
 
         // transitions to the facility's detail view
