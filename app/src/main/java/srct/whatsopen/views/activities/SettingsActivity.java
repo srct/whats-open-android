6d068974d75f52fca8dca5e337a7510a860fbb42
michael lundquist <michaellundquist7@gmail.com>
2018-10-04 10:23:17 -0400

    Adding a minor bug fix.

--- app/src/main/java/srct/whatsopen/views/activities/SettingsActivity.java
+++ app/src/main/java/srct/whatsopen/views/activities/SettingsActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import srct.whatsopen.MyApplication;
